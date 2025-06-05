b727a35a438ee3b91f55d4b45a3668d327036a35
chengqiwen777 <124917702+chengqiwen777@users.noreply.github.com>
2025-06-05 15:59:17 +0800

    Delete 张欣欣 directory

--- 张欣欣/week04-张欣欣/噪声接口调用.py
+++
@@ -1,8 +0,0 @@
-import cv2
-from skimage import util
-
-img=cv2.imread("lenna.png")
-noise_img=util.random_noise(img,mode="salt")
-cv2.imshow("noise_img",noise_img)
-cv2.imshow("img",img)
-cv2.waitKey(0)
