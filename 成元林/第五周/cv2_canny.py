ae1c68ac88571911c999bfe1463ff75586f9c109
chengqiwen777 <124917702+chengqiwen777@users.noreply.github.com>
2025-06-05 16:00:34 +0800

    Delete 成元林 directory

--- 成元林/第五周/cv2_canny.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-oriimg = cv2.imread("lenna.png")
-grayimg = cv2.cvtColor(oriimg,cv2.COLOR_BGR2GRAY)
-cny_img = cv2.Canny(grayimg,100,200,apertureSize=3)
-cv2.imshow("cny_img",cny_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
